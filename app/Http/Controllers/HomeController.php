79f40ddd7ac2cb014c03b7db638045fa91cad02d
Rambimo Regeng Pangestu <rambimo28@gmail.com>
2023-02-27 11:38:48 +0700

    commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends Controller
     // }
 
     public function index(){
-        return view ('layout.home');
+        return view ('layout.Layout.template');
     }
 
     public function product(){
