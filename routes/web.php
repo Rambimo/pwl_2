3941b8813340e0baa8040491ff8e535d79bd7b4d
Rambimo Regeng Pangestu <rambimo28@gmail.com>
2023-03-27 11:38:26 +0700

    auth

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,72 +18,83 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return ('selamat datang');
-// });
-// Route::get('/about', function(){
-//     return('Nim : 2141720094 - Nama : Rambimo Regeng Pangestu');
-// });
-// Route::get('/articles/{id}', function($id){
-//     return('Halaman Artikel dengan ID ' . $id);
-// });
 
 
-// Route::get('/', [PageController::class, 'index']);
-// Route::get('/about', [PageController::class, 'index2']);
-// Route::get('/articles/{id}', [PageController::class, 'index3']);
+Auth::routes();
+Route::get('/logout', [LoginController::class, 'logout']);
+
+Route::middleware(['auth'])->group(function() {
+    // Route::get('/', function () {
+    //     return ('selamat datang');
+    // });
+    // Route::get('/about', function(){
+    //     return('Nim : 2141720094 - Nama : Rambimo Regeng Pangestu');
+    // });
+    // Route::get('/articles/{id}', function($id){
+    //     return('Halaman Artikel dengan ID ' . $id);
+    // });
 
 
-// Route::get('/', [HomeController::class, 'index']);
-// Route::get('/about', [AboutController::class, 'index']);
-// Route::get('/articles/{id}', [ArticleController::class, 'index']);
+    // Route::get('/', [PageController::class, 'index']);
+    // Route::get('/about', [PageController::class, 'index2']);
+    // Route::get('/articles/{id}', [PageController::class, 'index3']);
 
 
-// Route::get('/home', [ContohController::class, 'index']);
-
-// Route::prefix('product')->group(function (){
-//     Route::get('/list', [ContohController::class, 'product']);
-// });
-
-// Route::get('/news{page}', [ContohController::class, 'news']);
-
-// Route::prefix('program')->group(function (){
-//     Route::get('/list', [ContohController::class, 'program']);
-// });
-
-// Route::resource('contact', ContohController::class);
+    // Route::get('/', [HomeController::class, 'index']);
+    // Route::get('/about', [AboutController::class, 'index']);
+    // Route::get('/articles/{id}', [ArticleController::class, 'index']);
 
 
-// Route::get('/home', [HomeController::class, 'index']);
+    // Route::get('/home', [ContohController::class, 'index']);
 
-// Route::prefix('home')->group(function (){
-//     Route::get('/product', [HomeController::class, 'product']);
-// });
+    // Route::prefix('product')->group(function (){
+    //     Route::get('/list', [ContohController::class, 'product']);
+    // });
 
-// Route::prefix('home')->group(function (){
-//     Route::get('/news', [HomeController::class, 'news']);
-// });
+    // Route::get('/news{page}', [ContohController::class, 'news']);
 
-// Route::prefix('home')->group(function (){
-//     Route::get('/program', [HomeController::class, 'program']);
-// });
+    // Route::prefix('program')->group(function (){
+    //     Route::get('/list', [ContohController::class, 'program']);
+    // });
 
-// Route::prefix('home')->group(function (){
-//     Route::get('/aboutus', [HomeController::class, 'aboutus']);
-// });
+    // Route::resource('contact', ContohController::class);
 
-// Route::prefix('home')->group(function (){
-//     Route::get('/contactus', [HomeController::class, 'contactus']);
-// });
 
-// Route::get('/', function () {
-//     return view ('layout.Layout.template');
-// });
+    // Route::get('/home', [HomeController::class, 'index']);
 
-Route::get('/dashboard', [DashboardController::class, 'index']);
-Route::get('/profile', [ProfileController::class, 'index']);
-Route::get('/pengalaman', [PengalamanKuliahController::class, 'index']);
-Route::get('/hobby', [HoobyController::class, 'index']);
-Route::get('/keluarga', [KeluargaController::class, 'index']);
+    // Route::prefix('home')->group(function (){
+    //     Route::get('/product', [HomeController::class, 'product']);
+    // });
 
-// Route::get('/kendaraan', [KendaraanController::class, 'index']);
+    // Route::prefix('home')->group(function (){
+    //     Route::get('/news', [HomeController::class, 'news']);
+    // });
+
+    // Route::prefix('home')->group(function (){
+    //     Route::get('/program', [HomeController::class, 'program']);
+    // });
+
+    // Route::prefix('home')->group(function (){
+    //     Route::get('/aboutus', [HomeController::class, 'aboutus']);
+    // });
+
+    // Route::prefix('home')->group(function (){
+    //     Route::get('/contactus', [HomeController::class, 'contactus']);
+    // });
+
+    // Route::get('/', function () {
+    //     return view ('layout.Layout.template');
+    // });
+
+    Route::get('/', [DashboardController::class, 'index']);
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+    Route::get('/profile', [ProfileController::class, 'index']);
+    Route::get('/pengalaman', [PengalamanKuliahController::class, 'index']);
+    Route::get('/hobby', [HoobyController::class, 'index']);
+    Route::get('/keluarga', [KeluargaController::class, 'index']);
+
+    // Route::get('/kendaraan', [KendaraanController::class, 'index']);
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+});
+
+
